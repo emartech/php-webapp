04eefc9dbf82071a2185f18709a57ac1e535b2a4
Attila Dröszler <attila.droszler@emarsys.com>
2019-12-06 15:24:05 +0100

    improve exception logging

--- src/Application/LoggerFactory.php
+++ src/Application/LoggerFactory.php
@@ -3,14 +3,12 @@
 namespace Emartech\Application;
 
 use Exception;
-use Monolog\Formatter\FormatterInterface;
 use Monolog\Formatter\JsonFormatter;
 use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
-use Throwable;
 
 class LoggerFactory
 {
@@ -35,31 +33,22 @@ class LoggerFactory
         }
     }
 
-    private function createFormatter(): FormatterInterface
+    private function createFormatter(): JsonFormatter
     {
         return new class extends JsonFormatter
         {
             public function format(array $record): string
             {
                 unset($record['datetime']);
-                if (isset($record['context']['exception'])) {
-                    $exception = $record['context']['exception'];
-                    if ($exception instanceof Throwable) {
-                        $record['context']['trace'] = $exception->getTraceAsString();
-                    }
-                }
                 return parent::format($record);
             }
         };
     }
 
-    /**
-     * @throws Exception
-     */
     private function createHandler(string $logLevel): HandlerInterface
     {
         $streamHandler = new StreamHandler('php://stderr', Logger::getLevels()[$logLevel]);
-        $streamHandler->setFormatter($this->createFormatter());
+        $streamHandler->setFormatter($this->createFormatter()->includeStacktraces());
         return $streamHandler;
     }
 }
